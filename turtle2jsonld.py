91c8c832a732909aea80f793c319b1055a6903b6
ThamerMECHARNIA <t.mecharnia@gmail.com>
2023-06-22 18:32:52 +0200

    new test

--- turtle2jsonld.py
+++ turtle2jsonld.py
@@ -8,7 +8,7 @@ def turtle2jsonld(turtle_file, jsonld_file):
     graph.parse(turtle_file, format='turtle')
 
     # Serialize the graph to JSON-LD format
-    jsonld_data = graph.serialize(format='json-ld', auto_compact=True, indent=4).encode('utf-8')
+    jsonld_data = graph.serialize(format='json-ld').encode('utf-8')
     graph.serialize(destination=jsonld_file, format='json-ld')
 
     return jsonld_data
